33b48c2b544c0a67b65ca6c434354035d37c5b22
Bjarni Jens Kristinsson <bjarni.jens@gmail.com>
2014-03-09 15:00:55 +0000

    Kafli um toString() aðferðina bætt við og smávægileg atriði í skýrslu löguð.

--- PrufuForrit.java
+++ PrufuForrit.java
@@ -20,30 +20,39 @@ public class PrufuForrit {
         
         //~ F: Búið er að lesa inn núll línur af staðalinntaki.
         while (scanner.hasNext()) {
-            //~ I: Búið er að lesa inn núll eða fleiri línur af staðalinntaki og gera viðeigandi aðgerðir með þær.
+            //~ I: Búið er að lesa inn núll eða fleiri línur af staðalinntaki 
+            //~    og gera viðeigandi aðgerðir með þær.
             String line = scanner.nextLine(); //tekur inntak fram að næsta enter
-            String[] S = line.split(" "); //býr til fylki af orðunum í línunni (skipt eftir bilum)
+            String[] S = line.split(" "); //fylki af orðunum í línunni (skipt eftir bilum)
             
             //~ Skoðum núna hvert fyrsta orðið í línunni er.
             switch (S[0]) {
                 case  "+": assert(S.length==3); // "+ a b"
-                            tree = AVLIntervalTree.insert( tree, new Interval( Integer.parseInt(S[1]) , Integer.parseInt(S[2]) ) );
+                            tree = AVLIntervalTree.insert( tree, new Interval(
+                                Integer.parseInt(S[1]) , Integer.parseInt(S[2]) ) );
                             break;
                 case  "-": assert(S.length==3); // "- a b"
-                            tree = AVLIntervalTree.delete( tree, new Interval( Integer.parseInt(S[1]) , Integer.parseInt(S[2]) ) );
+                            tree = AVLIntervalTree.delete( tree, new Interval(
+                                Integer.parseInt(S[1]) , Integer.parseInt(S[2]) ) );
                             break;
                 case "?o": assert(S.length==3); // "?o a b"
-                            System.out.println( treeToString( AVLIntervalTree.intersectInterval( tree , new Interval( Integer.parseInt(S[1]) , Integer.parseInt(S[2]) ) ) ) );
+                            System.out.println( treeToString(
+                                AVLIntervalTree.intersectInterval( tree , new Interval(
+                                    Integer.parseInt(S[1]) , Integer.parseInt(S[2]) ) ) ) );
                             break;
                 case "?i": assert(S.length==3); // "?i a b"
-                            System.out.println( treeToString( AVLIntervalTree.containInterval( tree , new Interval( Integer.parseInt(S[1]) , Integer.parseInt(S[2]) ) ) ) );
+                            System.out.println( treeToString(
+                                AVLIntervalTree.containInterval( tree , new Interval(
+                                    Integer.parseInt(S[1]) , Integer.parseInt(S[2]) ) ) ) );
                             break;
                 case "?p": assert(S.length==2); // "?p a"
-                            System.out.println( treeToString( AVLIntervalTree.containInteger( tree , Integer.parseInt(S[1]) ) ) );
+                            System.out.println( treeToString( AVLIntervalTree.containInteger(
+                                tree , Integer.parseInt(S[1]) ) ) );
                             break;
-                default: System.out.println("Input line in wrong format. Exiting now."); System.exit(0); break;
+                default: System.out.println("Error in input line."); System.exit(0); break;
             }
         }
-        //~ E: Búið er að lesa inn allar línur af staðalinntaki og gera viðeigandi aðgerðir með þær.
+        //~ E: Búið er að lesa inn allar línur af staðalinntaki
+        //~    og gera viðeigandi aðgerðir með þær.
     }
 }
